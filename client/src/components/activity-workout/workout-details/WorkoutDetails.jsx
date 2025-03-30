423f4029caf8df6fc85f79bbb849538d6ea5285f
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-03-30 14:58:00 +0300

    add direct link for googledrive functionality

--- client/src/components/activity-workout/workout-details/WorkoutDetails.jsx
+++ client/src/components/activity-workout/workout-details/WorkoutDetails.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router";
 import { useParams } from "react-router";
 import activityService from "../../../api/activityService";
 
@@ -14,8 +13,7 @@ export default function WorkoutDetails({
   videoLink,
   description,
 }) {
-  const [activity,setActivity] = useState({});
-  
+  const [activity, setActivity] = useState({});
   const { activityId } = useParams();
 
   useEffect(() => {
@@ -25,32 +23,52 @@ export default function WorkoutDetails({
     })();
   }, [activityId]);
 
+  // Function to convert Google Drive URL to direct image URL
+  const getDirectImageUrl = (url) => {
+    if (!url) {
+      return null;
+    }
+    const regex = /\/file\/d\/(.*)\/view/;
+    const match = url.match(regex);
+    if (match && match[1]) {
+      return `https://drive.google.com/uc?export=view&id=${match[1]}`;
+    }
+    return url; // Return original URL if not a Google Drive link
+  };
+
   return (
-    <>
-      <div className="container col-md-12">
-        <div id="ho_shad" className="activity_box  text_align_center">
-          <div className="col-md-8">
-            <h3>{activity.title}</h3>
-            <div>with {activity.instructor}</div>
-            <div className="details_box text_align_center">
-              <p>
-                <span>{activity.time} MIN</span> | <span>{activity.intensity}</span>
-              </p>
-            </div>
-            <p>{activity.description}</p>
+    <div className="container col-md-12">
+      <div id="ho_shad" className="activity_box text_align_center">
+        <div className="col-md-8">
+          <h3>{activity.title}</h3>
+          <div className="details_box text_align_center">
+            <p>
+              <span>{activity.time} MIN</span> |{" "}
+              <span>{activity.intensity}</span>
+            </p>
           </div>
-          <div className="col-md-8">
-            <figure>
-              <img src={activity.imageUrl} alt="#" />
-            </figure>
-            <div className="play_icon">
-              <a className="play-btn" href="javascript:void(0)">
-                <img src="/images/play_icon.png" />
-              </a>
-            </div>
+          <p>{activity.description}</p>
+        </div>
+
+        <div className="col-md-8">
+          <figure>
+            {activity.imageUrl ? (
+              <img
+                src={getDirectImageUrl(activity.imageUrl)}
+                alt={activity.title}
+              />
+            ) : (
+              <p>No image available</p>
+            )}
+          </figure>
+
+          <div className="play_icon">
+            <a className="play-btn" href="javascript:void(0)">
+              <img src="/images/play_icon.png" />
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
