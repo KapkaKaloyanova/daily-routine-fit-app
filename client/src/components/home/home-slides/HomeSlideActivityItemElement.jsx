4f7b6bb185f15aa2c02f942d3a4f55ab7ac7ea25
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-04-07 00:32:21 +0300

    Add direct link from recent activity to details page

--- client/src/components/home/home-slides/HomeSlideActivityItemElement.jsx
+++ client/src/components/home/home-slides/HomeSlideActivityItemElement.jsx
@@ -2,6 +2,7 @@ import processImageUrl from "../../../utils/processImageUrl";
 import styles from './home-slide-activity.module.css';
 
 export default function HomeSlideActivityItemElement({ latestActivities }) {
+  
   return (
     <>
       <div className="col-md-7">
@@ -9,7 +10,6 @@ export default function HomeSlideActivityItemElement({ latestActivities }) {
           {latestActivities.length > 0 
           ? (
             latestActivities.map((activity, index) => {
-              // Check valid URLs
               const processedImageUrl = processImageUrl(activity.imageUrl) 
 
               return (
@@ -22,6 +22,9 @@ export default function HomeSlideActivityItemElement({ latestActivities }) {
                         src={processedImageUrl} 
                         alt={activity.title} />
                     </figure>
+                    <div className="play_icon">
+                        <a className="play-btn" href={`/activity/${activity.category}/${activity._id}/details`}><img src="images/play_icon.png" /></a>
+                    </div>
                     <p className={styles.p}>{activity.description}</p>
                   </div>
                 </div>
